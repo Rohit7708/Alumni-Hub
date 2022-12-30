6088625fe210678bf7872c7ec09abe15ffbac940
Rohith <baburohit810@gmail.com>
2022-12-30 07:06:14 +0530

    alumni push

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('signUp/', views.signUp, name="signup"),
     path('logout/', views.logout, name="log"),
     path('postsignUp/', views.postsignUp),
-    path('navbar/',views.navbar,name="navbar"),
+    path('navbar/<session_id>/',views.navbar,name="navbar"),
+    path('alumni_login/',views.alumni_login,name="alumni_login"),
+    path('alumni_signup/',views.alumni_signup,name="alumni_signup"),
+    path('alumni_postsignUp/',views.alumni_postsignup),
+    path('alumni_postsignIn/',views.alumni_postsignin),
+    path('alumni_mainpage/',views.alumni_mainpage),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -32,7 +32,7 @@ def postsignIn(request):
         user=authe.sign_in_with_email_and_password(email,pasw)
         session_id=user['idToken']
         request.session['uid']=str(session_id)
-        return redirect('navbar')
+        return redirect('/navbar/{}'.format(session_id))
     except:
         message="Invalid Credentials!!Please ChecK your Data"
         return render(request,"Login.html",{"message":message})
@@ -60,14 +60,59 @@ def postsignUp(request):
         uid = user['localId']
         print(user)
         print(uid)
-        data={"name":name,"status":"1"}
+        data={"name":name,"role":"student"}
         print(data)
-        database.child("users").child(uid).child("details").set(data)
+        database.child("student").child(uid).child("details").set(data)
         return redirect('login')
      except:
         return render(request,"signUp.html")
      return render(request,"login.html")
 
-def navbar(request):
+def navbar(request,session_id):
 
     return render(request,"navbar.html")
+
+def alumni_login(request):
+
+    return render(request,'alumni_login.html')
+
+def alumni_postsignin(request):
+    email=request.POST.get('email')
+    pasw=request.POST.get('password')
+    try:
+        # if there is no error then signin the user with given email and password
+        user=authe.sign_in_with_email_and_password(email,pasw)
+        session_id=user['idToken']
+        request.session['uid']=str(session_id)
+        return redirect('navbar')
+    except:
+        message="Invalid Credentials!!Please ChecK your Data"
+        return render(request,"alumni_login.html",{"message":message})
+
+    return render(request,'alumni_mainpage.html')
+
+def alumni_signup(request):
+
+    return render(request,'alumni_signup.html')
+
+def alumni_postsignup(request):
+    email = request.POST.get('email')
+    passs = request.POST.get('password')
+    name = request.POST.get('name')
+
+    try:
+        user=authe.create_user_with_email_and_password(email,passs)
+        uid = user['localId']
+        data={"name":name,"role":"alumni"}
+        database.child("alumni").child(uid).child("details").set(data)
+        return redirect('alumni_login')
+
+    except:
+        return redirect('alumni_signup')
+
+
+    return render(request,'alumni_login.html')
+
+def alumni_mainpage(request):
+
+    return render(request,'alumni_mainpage.html')
